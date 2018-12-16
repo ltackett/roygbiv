5f3d58390157c2d1315f5d526afbb73e45839403
ltackett <lorin.tackett@gmail.com>
2018-12-16 01:44:47 -0800

    Removes key prop error

--- src/components/ChartSelector.js
+++ src/components/ChartSelector.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const ChartSelector = ({ charts }) => (
   <ul>
     {charts && charts.length && charts.map(chart =>
-      <li>
+      <li key={chart.id}>
         <Link to={`/charts/${chart.id}`} key={chart.id}>
           {chart.name}
         </Link>
